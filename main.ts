efc2b69471a8cd108fa5d51f30b90e945f6a8730
abrilgarza <70342563+abrilgarza@users.noreply.github.com>
2020-10-07 13:54:44 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+input.onButtonPressed(Button.A, function () {
+    for (let count = 0; count <= 4; count++) {
+        basic.showNumber(count)
+    }
+    basic.clearScreen()
+})
+let count = 0
+basic.forever(function () {
+	
+})
